a3a0fbfa4c4d7a64ba1d6e7eb470b6c02696401c
Radhika <sairadhika.iiitn@gmail.com>
2025-11-16 17:04:02 +0530

    terraform

--- data.tf
+++ data.tf
@@ -27,7 +27,7 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+  name = "/${var.project_name}/${var.environment}/private_subnet_id"
 }
 
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
